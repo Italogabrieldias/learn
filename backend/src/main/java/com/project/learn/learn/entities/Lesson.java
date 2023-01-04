3e11928a2c5fca42cadbf8f0eab3ae3142588ea8
Italo Gabriel <italogabrielde@gmail.com>
2023-01-04 18:09:32 -0300

    implement lesson, Content, task

---
+++ backend/src/main/java/com/project/learn/learn/entities/Lesson.java
@@ -0,0 +1,89 @@
+package com.project.learn.learn.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer position;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+    @ManyToMany
+    @JoinTable(name = "tb_lessons_done",
+            joinColumns = @JoinColumn(name = "lesson_id"),
+            inverseJoinColumns = {
+                    @JoinColumn(name = "user_id"),
+                    @JoinColumn(name = "offer_id")
+            })
+    private Set<Enrollment> enrollmentDone = new HashSet<>();
+
+    public Lesson() {
+    }
+
+    public Lesson(Long id, String title, Integer position, Section section) {
+        this.id = id;
+        this.title = title;
+        this.position = position;
+        this.section = section;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public Set<Enrollment> getEnrollmentDone() {
+        return enrollmentDone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lesson lesson)) return false;
+        return getId().equals(lesson.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -25,3 +25,7 @@ INSERT INTO tb_resource (title, description, position, img_Uri,type,offer_id) VA
 INSERT INTO tb_section (title, description, position, img_Uri,resource_id, prerequisite_id) VALUES ('Capitulo 01', 'neste capitulo vamos começar',1,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsAI056ak-kiMgt_Xl6FK0oNoSKu-8VY4Hs1-166RssEaZWtcytJNAr5E3duoDJeQz2dE&usqp=CAU', 1,NULL);
 INSERT INTO tb_section (title, description, position, img_Uri,resource_id, prerequisite_id) VALUES ('Capitulo 02', 'neste capitulo vamos evoluir',2,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsAI056ak-kiMgt_Xl6FK0oNoSKu-8VY4Hs1-166RssEaZWtcytJNAr5E3duoDJeQz2dE&usqp=CAU', 1,1);
 INSERT INTO tb_section (title, description, position, img_Uri,resource_id, prerequisite_id) VALUES ('Capitulo 03', 'neste capitulo vamos finalizar',3,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsAI056ak-kiMgt_Xl6FK0oNoSKu-8VY4Hs1-166RssEaZWtcytJNAr5E3duoDJeQz2dE&usqp=CAU', 1,2);
+
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment,refund_Moment,available, only_Update) VALUES (1,1,TIMESTAMP WITH TIME ZONE '2021-07-13T10:50:07.12345Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment,refund_Moment,available, only_Update) VALUES (2,1,TIMESTAMP WITH TIME ZONE '2021-07-13T10:50:07.12345Z', null, true, false);
